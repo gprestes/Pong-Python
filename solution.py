e321b06521f7967e2d83d10455bef6722faa9ab4
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2022-02-23 19:40:51 +0100

    Initial screen

---
+++ solution.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+def draw(win):
+    win.fill(BLACK)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
