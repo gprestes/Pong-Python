9352bd24721a99c103536dfcec46331f811531ab
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2022-02-24 18:05:25 +0100

    Added a ball in the middle of the field

--- solution.py
+++ solution.py
@@ -12,6 +12,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+BALL_RADIUS = 7
 
 
 class Paddle:
@@ -34,7 +35,26 @@ class Paddle:
             self.y += self.VEL
 
 
-def draw(win, paddles):
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+
+def draw(win, paddles, ball):
     win.fill(BLACK)
 
     for paddle in paddles:
@@ -44,6 +64,8 @@ def draw(win, paddles):
         if i % 2 == 1:
             continue
         pygame.draw.rect(win, WHITE, (WIDTH // 2 - 5, i, 10, HEIGHT // 20))
+
+    ball.draw(win)
 
     pygame.display.update()
 
@@ -73,6 +95,7 @@ def main():
     left_paddle = Paddle(
         10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT
     )
+
     right_paddle = Paddle(
         WIDTH - 10 - PADDLE_WIDTH,
         HEIGHT // 2 - PADDLE_HEIGHT // 2,
@@ -80,9 +103,11 @@ def main():
         PADDLE_HEIGHT,
     )
 
+    ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
+
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle])
+        draw(WIN, [left_paddle, right_paddle], ball)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
