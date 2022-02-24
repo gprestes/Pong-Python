60569b617f0e3156024439165fcd436eefb6da9d
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2022-02-24 17:19:04 +0100

    Add paddles

--- solution.py
+++ solution.py
@@ -11,9 +11,28 @@ FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
-def draw(win):
+
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+
+def draw(win, paddles):
     win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
     pygame.display.update()
 
 
@@ -21,9 +40,19 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
+    left_paddle = Paddle(
+        10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT
+    )
+    right_paddle = Paddle(
+        WIDTH - 10 - PADDLE_WIDTH,
+        HEIGHT // 2 - PADDLE_HEIGHT // 2,
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT,
+    )
+
     while run:
         clock.tick(FPS)
-        draw(WIN)
+        draw(WIN, [left_paddle, right_paddle])
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
