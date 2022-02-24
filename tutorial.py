ad46cac5b741f7ec185ec62e81b16ac94652c4fe
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2022-02-24 21:10:35 +0100

    Implement game with left player as human

---
+++ tutorial.py
@@ -0,0 +1,27 @@
+import pygame
+from pong import Game
+
+width, height = 700, 500
+window = pygame.display.set_mode((width, height))
+
+game = Game(window, width, height)
+
+run = True
+clock = pygame.time.Clock()
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        game.move_paddle(left=True, up=True)
+    if keys[pygame.K_s]:
+        game.move_paddle(left=True, up=False)
+
+    game_info = game.loop()
+    print(game_info.left_score, game_info.right_score)
+    game.draw()
+    pygame.display.update()
+pygame.quit()
